e05e43b122a11b521b28d494f1d3bf33a31faa4a
Ubuntu <ubuntu@ip-172-31-13-65.ap-northeast-1.compute.internal>
2025-08-28 14:47:24 +0000

    [Bookers2]いいね機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,11 @@
 class Book < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
