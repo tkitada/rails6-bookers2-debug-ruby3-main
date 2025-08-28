dc2a08a516f41f64d032d67e74a83af547efe3c9
Ubuntu <ubuntu@ip-172-31-13-65.ap-northeast-1.compute.internal>
2025-08-28 23:36:55 +0000

    [Bookers2/コメント機能]model, controller追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
   validates :title,presence:true
