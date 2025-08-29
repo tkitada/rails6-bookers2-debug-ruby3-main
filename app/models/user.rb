c1331c90b7b7049df35dc885c4fca1df9a4882fe
Ubuntu <ubuntu@ip-172-31-13-65.ap-northeast-1.compute.internal>
2025-08-29 09:53:48 +0000

    [Bookers2/フォロー機能]users/_index, users/_info viewへ機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :followered
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -21,5 +21,9 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  def following?(user)
+    followings.exists?(id: user.id)
   end
 end
